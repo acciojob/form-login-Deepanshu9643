e29625eb9ccabbdf043d93d2ca685091b5abd601
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-10 18:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
-
+	  // event.preventDefault();
+    
+    // Get the values directly from the form using document.querySelector
+    var firstName = document.querySelector('input[name="fname"]').value;
+    var lastName = document.querySelector('input[name="lname"]').value;
+    
+    // Display the full name in an alert
+    alert(firstName + " " + lastName);
+	
 }
+
